e7f249b7fd9afaf3e68228f7ce9c974fbb76d4b3
Ferdi <ferdivt@deltan-n.nl>
2019-11-09 12:27:21 +0100

    Cleanup

--- RoosterPlanner.Data/Context/Seed/CategorySeed.cs
+++ RoosterPlanner.Data/Context/Seed/CategorySeed.cs
@@ -15,10 +15,12 @@ namespace RoosterPlanner.Data.Context.Seed
 
         public override List<Category> Seed()
         {
-            List<Category> categories = new List<Category>();
-            categories.Add(new Category(Guid.NewGuid()) { Code = "KEUKEN", Name = "Keuken", LastEditBy = "System", LastEditDate = new DateTime(2019, 1, 22, 8, 1, 1) });
-            categories.Add(new Category(Guid.NewGuid()) { Code = "BEDIENING", Name = "Bediening", LastEditBy = "System", LastEditDate = new DateTime(2019, 1, 18, 16, 55, 29) });
-            categories.Add(new Category(Guid.NewGuid()) { Code = "LOGISTIEK", Name = "Logistiek", LastEditBy = "System", LastEditDate = new DateTime(2019, 1, 15, 2, 22, 55) });
+            List<Category> categories = new List<Category>
+            {
+                new Category(Guid.NewGuid()) { Code = "KEUKEN", Name = "Keuken", LastEditBy = "System", LastEditDate = new DateTime(2019, 1, 22, 8, 1, 1) },
+                new Category(Guid.NewGuid()) { Code = "BEDIENING", Name = "Bediening", LastEditBy = "System", LastEditDate = new DateTime(2019, 1, 18, 16, 55, 29) },
+                new Category(Guid.NewGuid()) { Code = "LOGISTIEK", Name = "Logistiek", LastEditBy = "System", LastEditDate = new DateTime(2019, 1, 15, 2, 22, 55) }
+            };
 
             this.modelBuilder.Entity<Category>().HasData(categories.ToArray());
             //List<Category> currentList = entitySet.Take(50).ToList();
