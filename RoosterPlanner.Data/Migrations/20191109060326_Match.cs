ef8b14cdb3d698e997eec5cbb073698ee38798f3
Ferdi <ferdivt@deltan-n.nl>
2019-11-09 07:07:16 +0100

    Added matches

---
+++ RoosterPlanner.Data/Migrations/20191109060326_Match.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RoosterPlanner.Data.Migrations
+{
+    public partial class Match : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Matches",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    LastEditBy = table.Column<string>(maxLength: 128, nullable: true),
+                    LastEditDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RowVersion = table.Column<byte[]>(rowVersion: true, nullable: true),
+                    ParticipationId = table.Column<Guid>(nullable: false),
+                    TaskId = table.Column<Guid>(nullable: false),
+                    Type = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Matches_Participations_ParticipationId",
+                        column: x => x.ParticipationId,
+                        principalTable: "Participations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Matches_Participations_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Participations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+             migrationBuilder.CreateIndex(
+                name: "IX_Matches_ParticipationId",
+                table: "Matches",
+                column: "ParticipationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_TaskId",
+                table: "Matches",
+                column: "TaskId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Matches");
+        }
+    }
+}
