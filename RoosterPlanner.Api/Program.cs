bbfb37d8aaa785f9af55947dad3a034399071fbd
JHAvanGestel <54802554+JHAvanGestel@users.noreply.github.com>
2020-09-30 11:13:45 +0200

    Chanced program.cs to work with Keyvault. NOT yet done

--- RoosterPlanner.Api/Program.cs
+++ RoosterPlanner.Api/Program.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.Hosting;
 
 namespace RoosterPlanner.Api
 {
@@ -19,9 +23,23 @@ namespace RoosterPlanner.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
-            return WebHost.CreateDefaultBuilder(args)
-                .UseIISIntegration()
-                .UseStartup<Startup>();
+            return WebHost.CreateDefaultBuilder(args).ConfigureAppConfiguration((context, config) =>
+            {
+                if (context.HostingEnvironment.IsProduction())
+                {
+                    var builtConfig = config.Build();
+
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var keyVaultClient = new KeyVaultClient(
+                        new KeyVaultClient.AuthenticationCallback(
+                            azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                    config.AddAzureKeyVault(
+                        $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/",
+                        keyVaultClient,
+                        new DefaultKeyVaultSecretManager());
+                }
+            }).UseIISIntegration().UseStartup<Startup>();
         }
     }
 }
