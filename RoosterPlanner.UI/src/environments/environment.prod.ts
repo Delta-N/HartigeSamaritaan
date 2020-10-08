23b3ccda6d03969632eae76dc4555e574386ab1a
JHAvanGestel <54802554+JHAvanGestel@users.noreply.github.com>
2020-10-08 12:54:04 +0200

    bugfix

--- RoosterPlanner.UI/src/environments/environment.prod.ts
+++ RoosterPlanner.UI/src/environments/environment.prod.ts
@@ -1,10 +1,8 @@
-import {b2cPolicies} from "../app/app-config";
-
 export const environment = {
   production: true,
   auth: {
     clientId: "a6ca557c-9d83-4867-a02d-99c5fbd159a5",
-    authority: b2cPolicies.authorities.signUpSignIn.authority,
+    authority: "https://DeltanHackaton.b2clogin.com/DeltanHackaton.onmicrosoft.com/b2c_1_susi",
     redirectUri: "https://roosterplanner-web-dev.azurewebsites.net",
     postLogoutRedirectUri: "https://roosterplanner-web-dev.azurewebsites.net",
     navigateToLoginRequestUrl: true,
