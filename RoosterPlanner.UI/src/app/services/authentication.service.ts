fd88968a24fcb870138c7e065c73438327d30b84
JHAvanGestel <54802554+JHAvanGestel@users.noreply.github.com>
2020-10-20 14:31:41 +0200

    added api service and authentication service

---
+++ RoosterPlanner.UI/src/app/services/authentication.service.ts
@@ -0,0 +1,80 @@
+import {Injectable, OnInit} from '@angular/core';
+import {BroadcastService, MsalService} from '@azure/msal-angular';
+import {isIE, b2cPolicies} from '../app-config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  isIframe = false;
+  loggedIn = false;
+
+  constructor(private broadcastService: BroadcastService, private authService: MsalService) {
+  }
+
+  ngOnInit(): void {
+    this.isIframe = window !== window.parent && !window.opener;
+    this.checkAccount();
+
+    this.broadcastService.subscribe('msal:loginSuccess', (success) => {
+
+      if (success.idToken.claims['tfp'] === b2cPolicies.names.resetPassword) {
+        window.alert("Password has been reset successfully. \nPlease sign-in with your new password");
+        return this.authService.logout();
+      }
+      this.checkAccount();
+    });
+
+    this.broadcastService.subscribe('msal:loginFailure', (error) => {
+      console.log('login failed');
+      console.log(error);
+
+      // Check for forgot password error
+      // Learn more about AAD error codes at https://docs.microsoft.com/en-us/azure/active-directory/develop/reference-aadsts-error-codes
+      if (error.errorMessage.indexOf('AADB2C90118') > -1) {
+        if (isIE) {
+          this.authService.loginRedirect(b2cPolicies.authorities.resetPassword);
+        } else {
+          this.authService.loginPopup(b2cPolicies.authorities.resetPassword);
+        }
+      }
+    });
+
+    // redirect callback for redirect flow (IE)
+    this.authService.handleRedirectCallback((authError, response) => {
+      if (authError) {
+        console.error('Redirect Error: ', authError.errorMessage);
+        return;
+      }
+
+      console.log('Redirect Success: ', response);
+    });
+  }
+
+  // other methods
+  checkAccount():boolean {
+    this.loggedIn = !!this.authService.getAccount();
+    return this.loggedIn;
+  }
+
+  login() {
+    if (isIE) {
+      this.authService.loginRedirect();
+    } else {
+      this.authService.loginPopup();
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  editProfile() {
+    if (isIE) {
+      this.authService.loginRedirect(b2cPolicies.authorities.editProfile);
+    } else {
+      this.authService.loginPopup(b2cPolicies.authorities.editProfile);
+    }
+  }
+}
+
---
+++ RoosterPlanner.UI/src/app/services/user.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from '@angular/core';
+import {HttpResponse} from "@angular/common/http";
+import {User} from "../models/user";
+import {AuthenticationService} from "./authentication.service";
+import {ApiService} from "./api.service";
+import {HttpRoutes} from "../helpers/HttpRoutes";
+import {JwtHelper} from "../helpers/jwt-helper";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  user: User = new User('');
+
+  constructor(private apiService: ApiService) {
+  }
+
+  async getUser(guid:string):Promise<User> {
+    await this.apiService.get<HttpResponse<User>>(`${HttpRoutes.personApiUrl}/${guid}`).toPromise().then(response => {
+      this.user = response.body
+    }).catch();
+      return this.user
+  }
+
+  //todo deze variablen zijn hardcoded
+  userIsAdminBackend(user:User):boolean{
+    if(user == null || user.userRole==null){return false;}
+    if(user.userRole=='Boardmember' || user.userRole=='Committeemember'){
+      return true;
+    }
+    return false;
+  }
+  userIsAdminFrontEnd():boolean{
+    const idToken = JwtHelper.decodeToken(sessionStorage.getItem('msal.idtoken'))
+    return ((idToken.extension_UserRole==1) || (idToken.extension_UserRole==2))
+  }
+}
