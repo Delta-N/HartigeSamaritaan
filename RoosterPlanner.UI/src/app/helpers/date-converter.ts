2dd8400e1e0f4009da7f0fdbbd070e0bed94c76e
JHAvanGestelDN <70514944+jhavangesteldn@users.noreply.github.com>
2020-10-23 13:24:58 +0200

    16599 - fix date bug

--- RoosterPlanner.UI/src/app/helpers/date-converter.ts
+++ RoosterPlanner.UI/src/app/helpers/date-converter.ts
@@ -1,7 +1,11 @@
 ﻿export class DateConverter {
+  static date: Date
 
-  static toDate (str) {
+  static toDate(str) {
     const [day, month, year] = str.split("-")
-    return new Date(year, month - 1, day)
+
+    this.date = new Date();
+    const offset = this.date.getTimezoneOffset();
+    return new Date(year, month - 1, day,offset*-1/60);
   }
 }
