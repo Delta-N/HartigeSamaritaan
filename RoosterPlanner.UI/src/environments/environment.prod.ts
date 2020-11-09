d162b790f35c7c2361bc333128f6b8648edf8381
Jaap van Gestel <jaapvg@delta-n.nl>
2020-11-09 11:49:06 +0100

    updated production settings

--- RoosterPlanner.UI/src/environments/environment.prod.ts
+++ RoosterPlanner.UI/src/environments/environment.prod.ts
@@ -10,11 +10,21 @@ export const environment = {
     knownAuthorities: ["https://DeltanHackaton.b2clogin.com"],
   },
   protectedResourceMap: [
-    ['https://localhost:5001/api/', ['https://DeltanHackaton.onmicrosoft.com/0f68eccd-b4a7-4747-b90e-ff88685173a3/Read']],
     ['https://roosterplanner-api-dev.azurewebsites.net/api/', ['https://DeltanHackaton.onmicrosoft.com/0f68eccd-b4a7-4747-b90e-ff88685173a3/Read']]
   ],
   scopes: [
     'https://DeltanHackaton.onmicrosoft.com/0f68eccd-b4a7-4747-b90e-ff88685173a3/Read'
   ],
+  authorities: {
+    signUpSignIn: {
+      authority: "https://DeltanHackaton.b2clogin.com/DeltanHackaton.onmicrosoft.com/b2c_1_susi"
+    },
+    resetPassword: {
+      authority: "https://DeltanHackaton.b2clogin.com/DeltanHackaton.onmicrosoft.com/b2c_1_reset_pwd"
+    },
+    editProfile: {
+      authority: "https://DeltanHackaton.b2clogin.com/DeltanHackaton.onmicrosoft.com/b2c_1_edit"
+    }
+  },
   backendUrl: "https://roosterplanner-api-dev.azurewebsites.net/"
 };
