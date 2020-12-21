ec193941099adb38a0faf70ec571edcf2074f321
Jaap van Gestel <jaapvg@delta-n.nl>
2020-12-21 11:17:34 +0100

    17152

--- RoosterPlanner.Api/Startup.cs
+++ RoosterPlanner.Api/Startup.cs
@@ -23,7 +23,9 @@ namespace RoosterPlanner.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #if DEBUG
             IdentityModelEventSource.ShowPII = true; // temp for more logging
+            #endif
 
             // Enable Application Insights telemetry collection.
             services.AddApplicationInsightsTelemetry(Configuration);
