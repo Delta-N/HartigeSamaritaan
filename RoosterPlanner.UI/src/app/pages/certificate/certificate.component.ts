8905e1d921ec738ea67503f04e58b43714e58a88
Jaap van Gestel <jaapvg@delta-n.nl>
2021-01-05 06:59:20 +0100

    able to get PDF certificates

--- RoosterPlanner.UI/src/app/pages/certificate/certificate.component.ts
+++ RoosterPlanner.UI/src/app/pages/certificate/certificate.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Certificate} from "../../models/Certificate";
 import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 import {CertificateService} from "../../services/certificate.service";
@@ -8,6 +8,8 @@ import {MatDialog} from "@angular/material/dialog";
 import {ToastrService} from "ngx-toastr";
 import {BreadcrumbService} from "../../services/breadcrumb.service";
 import {ConfirmDialogComponent, ConfirmDialogModel} from "../../components/confirm-dialog/confirm-dialog.component";
+import jsPDF from "jspdf";
+import {DateConverter} from "../../helpers/date-converter";
 
 @Component({
   selector: 'app-certificate',
@@ -19,6 +21,9 @@ export class CertificateComponent implements OnInit {
   certificate: Certificate
   isAdmin: boolean = false;
   loaded: boolean = false;
+
+  @ViewChild('pdfTable', {static: false}) pdfTable: ElementRef;
+
 
   constructor(private route: ActivatedRoute,
               private certificateService: CertificateService,
@@ -80,5 +85,67 @@ export class CertificateComponent implements OnInit {
         })
       }
     })
+  }
+
+  pdf() {
+    const doc = new jsPDF({orientation: "l", unit: "mm", format: [210, 297]});
+
+
+    let img = new Image()
+    img.src = "../../../assets/Logo.png"
+
+    let text:string;
+
+    doc.rect(10, 10, doc.internal.pageSize.width - 20, doc.internal.pageSize.height - 20, 'S');
+
+    let imgWidth:number = 100
+    doc.addImage(img, 'png', this.getOffSet(doc,imgWidth), 20, imgWidth, 35)
+
+    doc.setFont("Century Schoolbook")
+
+    doc.setFontSize(40)
+    text= "CERTIFICAAT"
+    doc.text(text, this.getOffSet(doc,this.getTextWidth(doc,text)), 80)
+
+    doc.setFontSize(16)
+    text = "Hiermee bevestigen wij dat:"
+    doc.text(text,  this.getOffSet(doc,this.getTextWidth(doc,text)), 90)
+
+    doc.setFontSize(24)
+    text= this.certificate.person.firstName + " " + this.certificate.person.lastName
+    doc.text(text, this.getOffSet(doc,this.getTextWidth(doc,text)),100)
+
+    doc.setFontSize(16)
+    text= "Met goed resultaat kunde heeft laten zien op het gebied van"
+    doc.text(text, this.getOffSet(doc,this.getTextWidth(doc,text)),110)
+
+    doc.setFontSize(24)
+    text= this.certificate.certificateType ? this.certificate.certificateType.name : "Onbekend"
+    doc.text(text, this.getOffSet(doc,this.getTextWidth(doc,text)),120)
+
+    doc.setFontSize(16)
+    if(this.certificate.certificateType?.level){
+      text= "Niveau behaald: " + this.certificate.certificateType.level
+      doc.text(text, this.getOffSet(doc,this.getTextWidth(doc,text)), 130)
+    }
+
+    text= "Afgiftedatum: " + DateConverter.toReadableStringFromDate(this.certificate.dateIssued)
+    doc.text(text, this.getOffSet(doc,this.getTextWidth(doc,text)), 150)
+
+
+    if(this.certificate.dateExpired)
+    {
+      text= "Verloopdatum: " + DateConverter.toReadableStringFromDate(this.certificate.dateExpired)
+      doc.text(text, this.getOffSet(doc,this.getTextWidth(doc,text)), 160)
+    }
+
+    doc.save('Certificaat.pdf');
+  }
+
+  getTextWidth(doc:jsPDF, text:string){
+    return doc.getStringUnitWidth(text) * doc.getFontSize() / doc.internal.scaleFactor;
+  }
+  getOffSet(doc:jsPDF, textWidth:number){
+    return (doc.internal.pageSize.width - textWidth) / 2;
   }
 }
