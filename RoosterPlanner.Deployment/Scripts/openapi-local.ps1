ff30f40d27af2c856187b53d84114eb7ec046e66
Jaap <jaapvg@delta-n.nl>
2024-03-01 21:38:12 +0100

    Update to .net8

--- RoosterPlanner.Deployment/Scripts/openapi-local.ps1
+++ RoosterPlanner.Deployment/Scripts/openapi-local.ps1
@@ -5,7 +5,7 @@ Set-Location -Path ../
 
 $env:NODE_TLS_REJECT_UNAUTHORIZED=0
 ng new openapi --create-application=false --package-manager=npm --skip-git --skip-tests
-Copy-Item ./RoosterPlanner.Api/bin/Debug/net7.0/open-api-spec.json ./openapi
+Copy-Item ./RoosterPlanner.Api/bin/Debug/net8.0/open-api-spec.json ./openapi
 Set-Location -Path ./openapi
 ng g library @RoosterPlanner/openapi
 Write-Output "export * from './lib'" > ./projects/rooster-planner/openapi/src/public-api.ts
