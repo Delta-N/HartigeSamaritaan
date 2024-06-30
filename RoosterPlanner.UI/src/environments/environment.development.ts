3f07611bca572ed884b1a5573367da02ecd5e48b
Jaap van Gestel <jaapvg@delta-n.nl>
2024-06-30 15:01:59 +0200

    Updated angular env. variables

--- RoosterPlanner.UI/src/environments/environment.development.ts
+++ RoosterPlanner.UI/src/environments/environment.development.ts
@@ -9,39 +9,15 @@ export const environment = {
 		clientId: '3cf2da13-f764-40f1-8f24-d6a3f81ff064',
 		authority:
 			'https://roosterplannerdev.b2clogin.com/roosterplannerdev.onmicrosoft.com/b2c_1_susi',
-		redirectUri: 'http://localhost:4200/',
-		postLogoutRedirectUri: 'http://localhost:4200/',
+		redirectUri: 'https://roosterplanner-web-dev.azurewebsites.net/',
+		postLogoutRedirectUri: 'https://roosterplanner-web-dev.azurewebsites.net/',
 		navigateToLoginRequestUrl: true,
 		validateAuthority: false,
 		knownAuthorities: ['https://roosterplannerdev.b2clogin.com'],
 	},
 	protectedResourceMap: [
 		[
-			'https://localhost:5001/api/',
-			[
-				'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
-			],
-		],
-		[
 			'https://roosterplanner-api-dev.azurewebsites.net/api/',
-			[
-				'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
-			],
-		],
-		[
-			'https://roosterplanner-api-prd.azurewebsites.net/api/',
-			[
-				'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
-			],
-		],
-		[
-			'https://roosterplanner-api-tst.azurewebsites.net/api/',
-			[
-				'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
-			],
-		],
-		[
-			'https://rooster-api.hartigesamaritaan.nl/api/',
 			[
 				'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
 			],
@@ -64,18 +40,9 @@ export const environment = {
 				'https://roosterplannerdev.b2clogin.com/roosterplannerdev.onmicrosoft.com/b2c_1_edit',
 		},
 	},
-	backendUrl: 'https://localhost:5001/',
+	backendUrl: 'https://roosterplanner-api-dev.azurewebsites.net/',
 
 	appInsights: {
-		instrumentationKey: '9001510e-6341-4bf0-b9c4-dbed80189dd8', //Dit is afhankelijk van de deploy omgeving
+		instrumentationKey: '9001510e-6341-4bf0-b9c4-dbed80189dd8',
 	},
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
--- RoosterPlanner.UI/src/environments/environment.ts
+++ RoosterPlanner.UI/src/environments/environment.ts
@@ -1,7 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
 	production: false,
 
@@ -18,30 +14,6 @@ export const environment = {
 	protectedResourceMap: [
 		[
 			'https://localhost:5001/api/',
-			[
-				'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
-			],
-		],
-		[
-			'https://roosterplanner-api-dev.azurewebsites.net/api/',
-			[
-				'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
-			],
-		],
-		[
-			'https://roosterplanner-api-prd.azurewebsites.net/api/',
-			[
-				'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
-			],
-		],
-		[
-			'https://roosterplanner-api-tst.azurewebsites.net/api/',
-			[
-				'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
-			],
-		],
-		[
-			'https://rooster-api.hartigesamaritaan.nl/api/',
 			[
 				'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
 			],
@@ -67,15 +39,6 @@ export const environment = {
 	backendUrl: 'https://localhost:5001/',
 
 	appInsights: {
-		instrumentationKey: '9001510e-6341-4bf0-b9c4-dbed80189dd8', //Dit is afhankelijk van de deploy omgeving
+		instrumentationKey: '9001510e-6341-4bf0-b9c4-dbed80189dd8',
 	},
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
