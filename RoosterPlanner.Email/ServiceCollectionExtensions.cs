4eb281fdaffc6bf05ad197d58e58e168f202e8c9
Trottero <wrlxs@hotmail.com>
2025-01-14 13:41:49 +0100

    Fix required service retrieval

--- RoosterPlanner.Email/ServiceCollectionExtensions.cs
+++ RoosterPlanner.Email/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 using Azure.Communication.Email;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace RoosterPlanner.Email;
 
@@ -9,13 +10,14 @@ public static class ServiceCollectionExtensions
     public static IHostApplicationBuilder AddEmailServices(this IHostApplicationBuilder builder)
     {
         builder.Services.AddTransient<IEmailService, AzureEmailServicesClient>();
+        builder.Services.Configure<ACSConfig>(builder.Configuration.GetSection(key: nameof(ACSConfig)));
+
         builder.Services.AddSingleton(sp =>
         {
-            var config = sp.GetRequiredService<ACSConfig>();
-            return new EmailClient(config.ConnectionString);
+            var config = sp.GetRequiredService<IOptions<ACSConfig>>();
+            return new EmailClient(config.Value.ConnectionString);
         });
 
-        builder.Services.Configure<ACSConfig>(builder.Configuration.GetSection(key: nameof(ACSConfig)));
 
         return builder;
     }
