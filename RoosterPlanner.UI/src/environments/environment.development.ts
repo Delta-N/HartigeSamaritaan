ff318f2c8203801fd27639aad9542cb7d8e317e7
Trottero <wrlxs@hotmail.com>
2025-01-16 10:55:35 +0100

    Add configuration for dev environment for angular

--- RoosterPlanner.UI/src/environments/environment.development.ts
+++ RoosterPlanner.UI/src/environments/environment.development.ts
@@ -3,46 +3,46 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	production: true,
+  production: true,
 
-	auth: {
-		clientId: '3cf2da13-f764-40f1-8f24-d6a3f81ff064',
-		authority:
-			'https://roosterplannerdev.b2clogin.com/roosterplannerdev.onmicrosoft.com/b2c_1_susi',
-		redirectUri: 'https://roosterplanner-web-dev.azurewebsites.net/',
-		postLogoutRedirectUri: 'https://roosterplanner-web-dev.azurewebsites.net/',
-		navigateToLoginRequestUrl: true,
-		validateAuthority: false,
-		knownAuthorities: ['https://roosterplannerdev.b2clogin.com'],
-	},
-	protectedResourceMap: [
-		[
-			'https://roosterplanner-api-dev.azurewebsites.net/api/',
-			[
-				'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
-			],
-		],
-	],
-	scopes: [
-		'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
-	],
-	authorities: {
-		signUpSignIn: {
-			authority:
-				'https://roosterplannerdev.b2clogin.com/roosterplannerdev.onmicrosoft.com/b2c_1_susi',
-		},
-		resetPassword: {
-			authority:
-				'https://roosterplannerdev.b2clogin.com/roosterplannerdev.onmicrosoft.com/b2c_1_reset_pwd',
-		},
-		editProfile: {
-			authority:
-				'https://roosterplannerdev.b2clogin.com/roosterplannerdev.onmicrosoft.com/b2c_1_edit',
-		},
-	},
-	backendUrl: 'https://roosterplanner-api-dev.azurewebsites.net/',
+  auth: {
+    clientId: '3cf2da13-f764-40f1-8f24-d6a3f81ff064',
+    authority:
+      'https://roosterplannerdev.b2clogin.com/roosterplannerdev.onmicrosoft.com/b2c_1_susi',
+    redirectUri: 'http://localhost:4200/',
+    postLogoutRedirectUri: 'http://localhost:4200/',
+    navigateToLoginRequestUrl: true,
+    validateAuthority: false,
+    knownAuthorities: ['https://roosterplannerdev.b2clogin.com'],
+  },
+  protectedResourceMap: [
+    [
+      'http://localhost:5003',
+      [
+        'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
+      ],
+    ],
+  ],
+  scopes: [
+    'https://roosterplannerdev.onmicrosoft.com/24e46ae1-6114-444b-87cc-788d5380f819/Read',
+  ],
+  authorities: {
+    signUpSignIn: {
+      authority:
+        'https://roosterplannerdev.b2clogin.com/roosterplannerdev.onmicrosoft.com/b2c_1_susi',
+    },
+    resetPassword: {
+      authority:
+        'https://roosterplannerdev.b2clogin.com/roosterplannerdev.onmicrosoft.com/b2c_1_reset_pwd',
+    },
+    editProfile: {
+      authority:
+        'https://roosterplannerdev.b2clogin.com/roosterplannerdev.onmicrosoft.com/b2c_1_edit',
+    },
+  },
+  backendUrl: 'http://localhost:5003/',
 
-	appInsights: {
-		instrumentationKey: '9001510e-6341-4bf0-b9c4-dbed80189dd8',
-	},
+  appInsights: {
+    instrumentationKey: '9001510e-6341-4bf0-b9c4-dbed80189dd8',
+  },
 };
