289458d40005f6e0f893c00d214df61682de0e32
Vincent van Soolen <vincentvs@delta-n.nl>
2025-09-24 17:09:53 +0200

    Formatting

--- RoosterPlanner.UI/src/app/pages/availability/availability.component.ts
+++ RoosterPlanner.UI/src/app/pages/availability/availability.component.ts
@@ -718,7 +718,6 @@ export class AvailabilityComponent implements OnInit, AfterViewInit {
       const child: HTMLElement = element.children[i] as HTMLElement;
       child.style.width = '26px';
       child.style.height = '26px';
-      child.style.fontSize = '16px !important';
       child.style.padding = '0px';
     }
   }
